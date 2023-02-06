6614d19919e79214c1101b69eed3b1a0f7c1161d
unknown <fsclaudio@gmail.com>
2023-02-05 22:30:40 -0300

    pdf

--- front/src/pages/FormStep3/index.tsx
+++ front/src/pages/FormStep3/index.tsx
@@ -1,21 +1,48 @@
 import { Theme } from '../../components/Theme';
 import { Link, useNavigate, } from 'react-router-dom';
 import * as C from './styles';
+import pdfMake from "pdfmake/build/pdfmake";
+import {useForm} from '../../Contexts/FormContext'
+import vfs from "pdfmake/build/vfs_fonts";
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
 
 
 
 export const FormStep3 = () => {
     const history = useNavigate(); 
-   
+    const {state} = useForm();
 
     const handleSave = ()=>{
         history('/step1')
      }
+     
      const handleDownload = ()=>{
-        
-        
-    }
+        pdfMake.vfs = vfs.pdfMake.vfs;
 
+        const reportTitle = [
+            'CONTRATO PAAS (CASHContratos) Nº 01 ',
+            
+   ];
+
+        const details = ['Pelo presente instrumento particular de Contrato de Licenciamento de uso de Software, na modalidade PaaS (Plataforma como Serviço), na melhor forma de direito fazem entre si:',
+     '(A)', state.BairroContratada, 'sociedade limitada, inscrita no Cadastro Nacional de Pessoas Jurídicas do Ministério da Fazenda (“CNPJ/MF”) sob o n°', state.CnpjContratada,', com sede na'
+      ,state.LogradouroContratada,' na Cidade de', state.CidadeContratada, 'e Estado de',state.EstadoContratada,' CEP', state.CepContratada,' inscrita no CNPJ/MF sob nº', state.CnpjContratada,
+      ', neste ato representada na forma do seu Contrato Social', state.nomeFantasiaContratada,];
+
+       const rodape = ['page=' + 1]
+
+        const docDefinitions: TDocumentDefinitions = {
+            pageSize: 'A4',
+            pageMargins: [15,50,15,40],
+
+            header:[reportTitle],
+            content: [details],
+            footer: [rodape]
+        }
+
+        pdfMake.createPdf(docDefinitions).download();
+    }
+    
     return( 
         <Theme>
         <C.Container>
